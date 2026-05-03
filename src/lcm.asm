2f4d7100a93b395ae91fbadebf8591094d34e367
ArcShahi <arcshahi54@gmail.com>
2026-05-03 11:31:02 +0530

    Fixed: Restores regs from correct memory offset

--- src/lcm.asm
+++ src/lcm.asm
@@ -1,4 +1,9 @@
-; Function for LCM. Will have to create .obj file or DLL and pass it to linker. 
+; Function for LCM. Will have to create .obj file or DLL and pass it to Linker. 
+; If a program uses LCM , It'll have to pass both : lcm.obj , gcd.obj to Linker Args 
+; using my script : neko .\test.asm -LinkerArgs "..\out\obj\gcd.obj","..\out\obj\lcm.obj"
+; The linker will take those obj files and create : test.exe 
+
+
 default rel
 extern gcd,lcm 
 
@@ -6,26 +11,28 @@ segment .text
 
 ; lcm(a,b)=|ab|/gcd(a,b)
 lcm :
-  ; Save paramters in shadow space
-  mov [rsp+0x08], rcx ; save a 
-  mov [rsp+0x0f], rdx ; save b 
 
+  ; Save paramters in shadow space ; offset 8B ( cuz caller's return addr was pushed )
+  mov [rsp+0x08], rcx ; save a 
+  mov [rsp+0x10], rdx ; save b 
+  push rbx 
   sub rsp,0x20 ; Shadow space : 32B ; Stack is unaligned by 8B 
 
   imul rcx,rdx
-  push rcx    ; ab on stack(cuz we're gonna call gcd and it may fuck the rcx ) , stack aligned now
+  mov rbx,rcx  ; rbx=ab (cuz we're gonna call gcd and it will fuck the rcx )
 
-  ; Fetch a,b original value 
-  mov rcx,[rsp+0x08]
-  mov rdx,[rsp+0x0f]
+  ; Fetch a,b original value; Now at offset of 40B
+  mov rcx,[rsp+0x30]
+  mov rdx,[rsp+0x38]
   call gcd
 
-  mov rcx,rax         ; rcx=gcd 
-  pop rax             ; rax= |ab| 
-  cqo                 ; Sign extend RCX->RDX 
-  idiv r8             ; |ab|/gcd(a,b)
+  mov rcx,rax         ; rcx=gcd(a,b) 
+  mov rax,rbx         ; rax= |ab| 
+  cqo                 ; Sign extend RAX->RDX 
+  idiv rcx            ; |ab|/gcd(a,b)
 
-  add rsp,0x20       
+  add rsp,0x20      
+  pop rbx 
   ret
 
   
