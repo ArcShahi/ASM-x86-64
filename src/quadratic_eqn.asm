65d5e57afc8b9c3057e622fdf262fc9098ef6b1f
ArcShahi <arcshahi54@gmail.com>
2026-05-13 18:23:44 +0530

    Updated to use AVX

--- src/quadratic_eqn.asm
+++ src/quadratic_eqn.asm
@@ -8,7 +8,7 @@ global main
 segment .data
   msg db "Coefficient (a,b,c): ",0
   fmt db "%lf %lf %lf",0
-  ans0 db "2 Real solutions: x1 = %g , x2= %g",0xA,0xA,0 
+  ans0 db "2 Real solutions: x1 = %g , x2 = %g",0xA,0xA,0 
   ans1 db "Double solution : x1 = x2 = %g",0xA,0xA,0 
   ans2 db "2 Complex solutions : x1 = x2 = ( %g +- %gi )", 0xA,0xA, 0
 
@@ -20,57 +20,52 @@ segment .text
 ; xmm0=a,xmm1=b,xmm2=c 
 find_roots:
   sub rsp,0x28 
+
+  vmulsd xmm3,xmm0,xmm2 
+  vmulsd  xmm3,xmm3,[neg4]     ; xmm3= -4ac 
+  vmulsd  xmm2,xmm1,xmm1       ; xmm2 = b^2 
+  vaddsd  xmm3,xmm3,xmm2       ; xmm3= descriminant = b^2 +(-4ac) 
+  vsqrtsd xmm2,xmm2,xmm3       ; xmm2= sqrt(d) 
   
-  movapd xmm3,xmm0   ; xmm3 = a 
-  mulsd  xmm3,xmm2 
-  mulsd  xmm3,[neg4] ; xmm3= -4ac 
-  movapd xmm2,xmm1  
-  mulsd  xmm2,xmm1  ;  xmm2= b^2 
-  addsd  xmm3,xmm2  ;  xmm3= descriminant = b^2 +(-4ac) 
-  sqrtsd xmm2,xmm3  ;  xmm2= sqrt(d) 
-  
-  mulsd xmm1,[neg1] ;  xmm1 = -b
-  addsd xmm0,xmm0   ;  xmm0 = 2a
-  xorpd xmm4,xmm4  
-  comisd xmm3,xmm4  ; compare descriminant with 0 
+  vmulsd xmm1,xmm1,[neg1]      ; xmm1 = -b
+  vaddsd xmm0,xmm0,xmm0        ; xmm0 = 2a
+  vxorpd xmm4,xmm4,xmm4  
+  vcomisd xmm3,xmm4            ; compare descriminant with 0 
   jb .LCR
   jg .LRR
 
   ; Double Solution , 1 Real root 
-  divsd xmm1,xmm0   ; x1=x2= -b/2a 
+  vdivsd xmm1,xmm1,xmm0         ; x1=x2= -b/2a 
   lea rcx,[ans1]
-  movq rdx,xmm1     ; Mirror in GP reg , cuz printf and x64 ABI is weird 
+  vmovq rdx,xmm1                ; Mirror in GP reg , cuz printf and x64 ABI is weird 
   call printf 
   jmp .done
 
  ; 2 Real solution 
 .LRR:
-  movapd xmm4,xmm1 ; xmm4 = -b 
-  addsd  xmm1,xmm2 
-  divsd  xmm1,xmm0 ; xmm1= x1=  (-b+sqrt(d))/ 2a
-
-  subsd xmm4,xmm2
-  divsd xmm4,xmm0 
-  movapd xmm2,xmm4 ; xmm2= x2 = (-b-sqrt(d)) / 2a 
-
+  vmovapd xmm4,xmm1             ; xmm4 = -b  
+  vaddsd xmm1,xmm1,xmm2 
+  vdivsd xmm1,xmm1,xmm0         ; xmm1= x1 =  (-b+sqrt(d))/ 2a )
+  vsubsd xmm2,xmm4,xmm2
+  vdivsd xmm2,xmm2,xmm0         ; xmm2 = x2 = (-b-sqrt(d) / 2a ) 
+  
   lea rcx,[ans0]
-  movq rdx,xmm1 
-  movq r8,xmm2     ; mirroring 
+  vmovq rdx,xmm1 
+  vmovq r8,xmm2                 ; mirroring 
   call printf  
   jmp .done 
 
  ; 2 Complex Solution 
 .LCR:
-  movapd xmm4,xmm1 ; xmm4=-b 
-  divsd  xmm1,xmm0  ; xmm1 = real part = -b/2a 
-
-  mulsd xmm3,[neg1] ; Here 'd' is neg , so make it positive for sqrt 
-  sqrtsd xmm2,xmm3  ; xmm2 = sqrt(|d|) 
-  divsd xmm2,xmm0  ;  xmm2 = ur gf part = sqrt(|d|)/2a 
+  vmovapd xmm4,xmm1              ; xmm4=-b
+  vdivsd xmm1,xmm1,xmm0          ; xmm1 = real part = -b/2a 
+  vmulsd xmm2,xmm3,[neg1]        ; 'd' is -ive here, so making it positive 
+  vsqrtsd xmm2,xmm2,xmm2         ; xmm2 = sqrt(|d|)
+  vdivsd xmm2,xmm2,xmm0          ; xmm2 = ur gf part = sqrt(|d|)/ 2a 
   
-  lea rcx,[ans2]
-  movq rdx,xmm1  
-  movq r8 ,xmm2   ; Mirroring 
+  lea   rcx,[ans2]
+  vmovq rdx,xmm1  
+  vmovq r8 ,xmm2                  ; Mirroring 
   call printf 
 
 
@@ -80,7 +75,7 @@ find_roots:
   ret 
 
 main: 
-  sub rsp,0x38 ; 56B : 32B shadow + 24B Local and 8B caller ret addr = 64 B %16==0 Aligned 
+  sub rsp,0x38 ; 56B : 32B shadow + 24B Local and 8B caller ret addr = 64B % 16== 0 Aligned 
   call _CRT_INIT
 
   lea rcx,[msg]
@@ -93,9 +88,9 @@ main:
   call scanf 
 
   ; Pass values 
-  movsd xmm0,qword[rsp+0x30]
-  movsd xmm1,qword[rsp+0x28]
-  movsd xmm2,qword[rsp+0x20]
+  vmovsd xmm0,qword[rsp+0x30]
+  vmovsd xmm1,qword[rsp+0x28]
+  vmovsd xmm2,qword[rsp+0x20]
   call find_roots
 
 
@@ -103,5 +98,5 @@ main:
   add rsp,0x38
   ret 
 
-; This is one the worst things I've ever done...
+; This is one of the worst things I've ever done...
 ; -Shahi
