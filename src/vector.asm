a027815af07bae42baa9ad48e1e05e57758727e8
ArcShahi <arcshahi54@gmail.com>
2026-06-01 11:59:30 +0530

    Fixed: Alignmnet issues and struct return address

--- src/vector.asm
+++ src/vector.asm
@@ -1,57 +1,62 @@
 ; SIMD Vector operations : Add , Scale , Dot , cross 
 ; AVX with VEX.128 encoding version
 ; AVX cuts us some slacking... the memory for load and operations  don't need to be 16B or 32B aligned 
+; But I'm still making struct aligned (16B) 
 
-; Vector from C++ perspective : struct Vec3{float x{},y{},z{}}; struct Vec4{float x{},y{},z{},w{}}; 
+; Vector from C++ perspective : 
+; struct alignas(16) Vec3{float x{},y{},z{}}; 
+; struct alignas(16) Vec4{float x{},y{},z{},w{}}; 
 
 default rel
 
-global Vec3_add,Vec4_add,Vec3_scale,Vec4_scale,Vec3_dot,Vec4_dot
+global Vec3_add,Vec4_add,Vec3_scale,Vec4_scale,Vec3_dot,Vec4_dot,cross_product
 
 export Vec3_add
 export Vec3_scale
 export Vec4_scale
 export Vec3_dot
 export Vec4_dot
-
+export Vec4_add
+export cross_product
 
 segment .text
 
-; Vec3 add(Ve3& u,Vec3& v)
+; void add(Vec3& dest,Ve3& u,Vec3& v)
 Vec3_add:
- vmovups xmm0,[rdx]
- vmovups xmm1,[r8]
+ vmovaps xmm0,[rdx]
+ vmovaps xmm1,[r8]
  vaddps xmm0,xmm0,xmm1 
- vmovups [rcx],xmm0 
+ vmovaps [rcx],xmm0 
  ret 
 
-; It's already 16B Aligned
-; Vec4 add(Vec4& u,Vec4& v)
+; void add(Vec4& dest, Vec4& u,Vec4& v)
 Vec4_add:
   vmovaps xmm0,[rdx]
   vaddps xmm0,xmm0,[r8] 
   vmovaps [rcx],xmm0 
   ret 
 
- ;Vec3 scale(Vec3& u,float scale)
+ ; void scale(Vec3& dest, Vec3& u,float scale)
 Vec3_scale:
   vmovups xmm0,[rcx]
   vbroadcastss xmm1,xmm1 
   vmulps xmm0,xmm0,xmm1
+  vmovaps [rcx],xmm0 
   ret
 
-; Vec4 scale(Vec4& u,float scale)
+; void scale(Vec4& dest, Vec4& u,float scale)
 Vec4_scale:
   vmovaps xmm0,[rcx]
   vbroadcastss xmm1,xmm1 
   vmulps xmm0,xmm0,xmm1
+  vmovaps [rcx],xmm0 
   ret 
 
 ; float dot(Vec3& u,Vec3& v)
 Vec3_dot:
-  vmovups xmm0,[rcx]
-  vmovups xmm1,[rdx]
-  vdpps xmm0,xmm0,xmm1,0x71 ; 0b0111_0001 , Dot product of 3 lanes and store result in xmm0's lane 0 
+  vmovaps xmm0,[rcx]
+  vmovaps xmm1,[rdx]
+  vdpps xmm0,xmm0,xmm1,0x71   ; 0b0111_0001 , Dot product of 3 lanes and store result in xmm0's lane 0 
   ret 
 
 ; float dot(Vec4& u,Vec4& v)
@@ -60,10 +65,10 @@ Vec4_dot:
   vdpps xmm0,xmm0,[rdx],0xF1  ; 0b1111_0001 ; Dot product all lanes and store result in xmm0's lane 0 
   ret 
   
-; Vec3 cross(Vec3& dest,Vec3& u,Vec3& v)
+; void cross(Vec3& dest,Vec3& u,Vec3& v)
 cross_product:
-  vmovups xmm1,[rdx]        
-  vmovups xmm2,[r8]
+  vmovaps xmm1,[rdx]        
+  vmovaps xmm2,[r8]
 
   vshufps xmm1,xmm1,xmm1,0x52 ; 0b0101_0010 ; xmm1={z,x,y,y} 
   vshufps xmm2,xmm2,xmm2,0x09 ; 0b0000_1001 ; xmm2={y,z,x,x}
@@ -71,6 +76,6 @@ cross_product:
 
   vshufps xmm1,xmm1,xmm1,0x52 ; same mask : rotate right xmm1={y,z,x,x}
   vshufps xmm2,xmm2,xmm2,0x09 ; same mask : rotate left  xmm2={z,x,y,y}
-  vfmsub231ps xmm0,xmm1,xmm2    ; xmm0 = xmm1*xmm2 - xmm0 : Cross product 
-  vmovups [rcx],xmm0 
+  vfmsub231ps xmm0,xmm1,xmm2  ; xmm0 = xmm1*xmm2 - xmm0 : Cross product 
+  vmovaps [rcx],xmm0 
   ret 
