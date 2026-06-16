6f15fdc55b1f4e0dbe674f7132328ca9d1fdf490
ArcShahi <arcshahi54@gmail.com>
2026-06-16 11:17:24 +0530

    Fixed: Dot product for scalar tail

--- simd/array_dp.asm
+++ simd/array_dp.asm
@@ -8,10 +8,13 @@ segment .text
 ; float array_dp(float* A,float* B,int N)
 array_dp:
 
+  push rsi
+  push rdi 
+
   ; calculate loop iteration 
   mov r9d,r8d 
   and r8d,7   ; r8d=  N % 8 
-  and r9d,-8  ; r9d = N / 8
+  and r9d,-8  ; r9d = (N/8)*8 ; Vectorize count 
 
   vxorps ymm0,ymm0,ymm0 
   xor eax,eax ; counter 
@@ -21,25 +24,30 @@ array_dp:
   vfmadd231ps ymm0,ymm1,[rdx+rax*4]
   add eax,8 ;  8 elements forward
   cmp eax,r9d 
-  jl .loop
+  jb .loop
   
   vhaddps ymm0,ymm0,ymm0;
   vhaddps ymm0,ymm0,ymm0   ; yes.. we've to do it twice for proper dot product
-  vextractf128 xmm1,ymm0,1 ; Pull the upper 128 bit in xmm1 
+  vextractf128 xmm1,ymm0,0x01 ; Pull the upper 128 bit in xmm1 
   vaddps xmm0,xmm0,xmm1    ; current dot 
 
  ; Check if any remaining elements
- test r8d,r8d 
- jz .done
+  test r8d,r8d 
+  jz .done
 
   ; Handle remaining  
+  lea rsi,[rcx+r9*4]
+  lea rdi,[rdx+r9*4]
+  xor eax,eax ; i 
 .loop2:
-  vmovss xmm1,[rcx+rax*4]
-  vfmadd231ss xmm0,xmm1,[rdx+rax*4] ; xmm0 = xmm1 * mem128 + xmm0 
+  vmovss xmm1,[rsi+rax*4]
+  vfmadd231ss xmm0,xmm1,[rdi+rax*4] ; xmm0 = xmm1 * mem128 + xmm0 
   inc eax 
   cmp eax,r8d 
-  jl .loop2 
+  jb .loop2 
 
 .done:
- ret 
+  pop rdi
+  pop rsi 
+  ret 
   
