c04e329d0d7df2bef51435294b166d339e84cef8
ArcShahi <arcshahi54@gmail.com>
2026-08-01 09:30:37 +0530

    Renamed function

--- samples/memset.asm
+++ samples/memset.asm
@@ -10,11 +10,10 @@ segment .data
   ans db "Before : %d , After : %d",0xA,0
   val dd 69420 
 
-
 segment .text
   
-;memset(rcx=*dest,edx=value,r8=count)
-memset:
+;memoryset(rcx=*dest,edx=value,r8=count)
+memoryset:
   
   test r8,r8
   jz .done
@@ -28,7 +27,6 @@ memset:
  .done:
    ret
 
-
 main:
   push rbp
   sub rsp,0x20
@@ -38,13 +36,14 @@ main:
   lea rcx,[val]
   mov edx,0 
   mov r8 ,4 
-  call memset 
+  call memoryset 
 
   lea rcx,[ans]
   mov edx,ebp 
   mov r8d,[val]
   call printf 
 
+  xor eax,eax
   add rsp,0x20 
   pop rbp
   ret
