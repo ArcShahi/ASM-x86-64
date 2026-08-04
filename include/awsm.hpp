829cde370f4a521dee10926246f3322ce6e06177
ArcShahi <arcshahi54@gmail.com>
2026-08-04 19:24:19 +0530

    Header for Linear Algebra

---
+++ include/awsm.hpp
@@ -0,0 +1,30 @@
+#ifdef AWSM_HPP
+#define AWSM_HPP
+
+namespace awsm {
+
+struct alignas(16) Vec3 {
+  float x{},y{},z{},w{};
+};
+
+struct alignas(16) Vec4 {
+  
+  float x{},y{},z{},w{};
+};
+  
+// 3x3 Matrix : Row-major
+struct Max3x3 {
+  
+  Vec3 r[3]{};
+};
+
+struct Mat4x4 {
+  
+  Vec4 r[4]{};  
+};
+
+// TODO : Add function forward delclaration
+
+}
+
+#endif 
