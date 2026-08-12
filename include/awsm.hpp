f820d4f6e6d29f0fe3aa17f44fc03ead25c19647
ArcShahi <arcshahi54@gmail.com>
2026-08-12 19:13:22 +0530

    Tested : vec3_add,vec3_dot

--- include/awsm.hpp
+++ include/awsm.hpp
@@ -1,33 +1,35 @@
 #ifndef AWSM_HPP
 #define AWSM_HPP
 
-// Sample header 
+// Sample header
 
 namespace awsm {
 
-struct alignas(16) Vec3 {
-  float x{},y{},z{};
-};
+	struct alignas(16) vec3 {
+		float x{}, y{}, z{};
+	};
 
-struct alignas(16) Vec4 {
-  
-  float x{},y{},z{},w{};
-};
-  
-// 3x3 Matrix : Row-major
-struct Max3x3 {
-  
-  Vec3 r[3]{};
-};
+	struct alignas(16) vec4 {
+		float x{}, y{}, z{}, w{};
+	};
 
-struct Mat4x4 {
-  
-  Vec4 r[4]{};  
-};
+	// 3x3 Matrix : Row-major
+	struct max3x3 {
+		vec3 r[3]{};
+	};
 
+	struct mat4x4 {
+		vec4 r[4]{};
+	};
+
+	// Function declaration
+
+	// vec3 addition
+	extern "C" void vec3_add(awsm::vec3* dest, awsm::vec3* v, awsm::vec3* u);
+
+	// vec3 dot product
+	extern "C" float vec3_dot(awsm::vec3* v, awsm::vec3* u);
 }
 
 
-// TODO : Add function forward delclaration , can't put them inside namespace ,
-
-#endif 
+#endif
